7e466c15261d6ebac40e49d8a2b4b01eecf3f320
Marcelo Silveira <marcelosj33@gmail.com>
2024-10-08 11:33:51 -0300

    feat: fix duplicate x in modal close

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -6,7 +6,6 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "./_private/dialog";
-import { Icon } from "./icon";
 
 interface ModalProps {
 	isOpen: boolean;
@@ -26,15 +25,7 @@ export const Modal: React.FC<ModalProps> = ({
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>{title}</DialogTitle>
-					<DialogClose asChild>
-						<button
-							className="absolute top-4 right-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-white data-[state=open]:text-muted-foreground"
-							onClick={onClose}
-						>
-							<Icon name="X" className="h-4 w-4" />
-							<span className="sr-only">Close</span>
-						</button>
-					</DialogClose>
+					<DialogClose asChild />
 				</DialogHeader>
 				<div className="relative">{children}</div>
 			</DialogContent>
