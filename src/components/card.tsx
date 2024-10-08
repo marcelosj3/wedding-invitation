a9b133b31f6d2b640eb171329479f94f06709c8a
Marcelo Silveira <marcelosj33@gmail.com>
2024-10-08 10:38:50 -0300

    feat: improve card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,15 +12,14 @@ export const Card = ({ title, children, backgroundImageHref }: Props) => {
   return (
     <div className="bg-green-350 rounded-md relative overflow-hidden">
       {backgroundImageHref &&
-        <div className="absolute z-10 -right-12 -top-4">
-          <img src={backgroundImageHref} alt={title} className="w-full h-auto opacity-20" />
+        <div className="absolute z-10 -right-12 -top-4 pointer-events-none h-full">
+          <img src={backgroundImageHref} alt={title} className="w-full opacity-20 h-full" />
         </div>
       }
-      <section className="flex flex-col p-2 bg-green-350 rounded-md gap-1 opacity-100">
-        <div className="z-20">
-          <h2 className="text-2xl font-bold mb-4 text-gray-200">{title}</h2>
-          {children}
-        </div>
+
+      <section className="flex flex-col p-2 bg-green-350 rounded-md gap-2 opacity-100 h-full z-20">
+        <h2 className="text-2xl font-bold mb-2 text-gray-200">{title}</h2>
+        {children}
       </section>
     </div>
   );
